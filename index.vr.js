cc1247c24d82cbee733855d3a1f6e4f9ed451a1c
spawn <gandam00.transam@gmail.com>
2018-01-14 11:08:37 +0900

    init

---
+++ index.vr.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+export default class cps_lab_tour extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('theta.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          hello
+        </Text>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('cps_lab_tour', () => cps_lab_tour);
