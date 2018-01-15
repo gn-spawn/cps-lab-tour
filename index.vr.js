05ad015f716f6268796a303a6e479d2319f935f9
spawn <gandam00.transam@gmail.com>
2018-01-15 19:21:07 +0900

    だいたい作った

--- index.vr.js
+++ index.vr.js
@@ -6,30 +6,13 @@ import {
   Text,
   View,
 } from 'react-vr';
+import { LiveTour } from 'live-tour-lab';
+
 
 export default class cps_lab_tour extends React.Component {
   render() {
     return (
-      <View>
-        <Pano source={asset('lab1.jpg')}
-        style={{
-          transform: [{rotateY: -80}]
-        }}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
+      <LiveTour tourURI='hello-world.json' />
     );
   }
 };
